683170334ec9944a7c4842b9cde10e372c875ee4
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2013-07-10 08:18:20 -0500

    Changed version and product name.

--- AdobeCS6CrackAssistant/Properties/AssemblyInfo.cs
+++ AdobeCS6CrackAssistant/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A helpful tool to assist in cracking Adobe's CS6 products.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Terrasoft")]
-[assembly: AssemblyProduct("Photoshop CS6 Crack Assistant")]
+[assembly: AssemblyProduct("Adobe CS6 Crack Assistant")]
 [assembly: AssemblyCopyright("Copyright © 2013 Terrasoft")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.5.0")]
+[assembly: AssemblyFileVersion("1.2.5.0")]
